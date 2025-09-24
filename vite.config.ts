51bf7c687dda95290be7602eec0f70a573479a9e
Thomas de Saint Exupery <thomas.desaintexupery@doctolib.com>
2025-09-24 18:36:50 +0200

    fix name project

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import { VitePWA } from 'vite-plugin-pwa';
 import path from 'path';
 
 export default defineConfig({
-  base: process.env.NODE_ENV === 'production' ? '/calculearn/' : './',
+  base: process.env.NODE_ENV === 'production' ? '/CalcuLearn/' : './',
   plugins: [
     react(),
     VitePWA({
